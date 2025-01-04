76fa7f47edb097a4cee43a6648b1cf4f13446ba4
Andy <andywu.england@gmail.com>
2025-01-04 09:57:05 +0800

    chore: Add ci cd workflow

---
+++ ios/App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Andy Wu on 2025/1/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
