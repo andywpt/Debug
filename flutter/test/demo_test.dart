e04b1d3b49302fef7c987a8aa1179248a366a039
Andy <andywu.england@gmail.com>
2025-01-05 16:07:47 +0800

    ok

---
+++ flutter/test/demo_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final List<int> numbers = [11, 12, 13, 14, 15];
+  numbers.asMap().entries.forEach((entry) {
+    print("key: ${entry.key} value: ${entry.value}");
+  });
+  Map<String, int> hashMap = {};
+  hashMap['c'] = 3;
+  hashMap['a'] = 1;
+  hashMap['b'] = 2;
+
+  for (var entry in hashMap.entries) {
+    print('${entry.key}: ${entry.value}');
+  }
+  // group('1+1=2', () {
+  //   test('addition test', () {
+  //     expect(1 + 1, 2);
+  //   });
+  // });
+}
